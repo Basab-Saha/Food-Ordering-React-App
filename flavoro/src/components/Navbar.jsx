8217a5e4a6771398d49388c0b927498b032f8949
Basab Saha <ronylovescoding999@gmail.com>
2024-01-07 14:07:18 +0530

    Installed tailwind css and created Navbar

---
+++ flavoro/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex flex-col lg:flex-row justify-between mx-10 py-7'>
+        <div>
+            <h1 className='text-2xl font-bold'>{new Date().toUTCString().slice(0,16)}</h1>
+            <h3 className='text-3xl font-mono'>Flavoro Foods</h3>
+        </div>
+
+        <div>
+            <input className='border p-3 border-gray-500 w-full lg:w-[30vw]' type='search' name='search' placeholder='Search here'/>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar

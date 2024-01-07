43a651baa1801e3ddc19ad18b708c877241a262a
Basab Saha <ronylovescoding999@gmail.com>
2024-01-07 15:51:52 +0530

    FoodCard Component styled 😉

---
+++ flavoro/src/components/FoodList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import FoodCard from './FoodCard'
+
+const FoodList = () => {
+  return (
+    <FoodCard/>
+  )
+}
+
+export default FoodList
--- flavoro/src/pages/Home.jsx
+++ flavoro/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import CatagoryList from '../components/CatagoryList'
+import FoodList from '../components/FoodList'
 
 const Home = () => {
   return (
    <>
    <Navbar/>
    <CatagoryList/>
+   <FoodList/>
    </>
   )
 }
